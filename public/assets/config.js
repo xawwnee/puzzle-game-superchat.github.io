72cdb2401b7e7a2be87f8037552bb05e1eceda02
xawwnee <76479202+xawwnee@users.noreply.github.com>
2025-07-28 19:23:12 +0530

    Delete public/assets directory

--- public/assets/config.js
+++
@@ -1,7 +0,0 @@
-// Development defaults - overwritten in production
-window.gameConfig = {
-  PEER_HOST: "localhost",
-  PEER_PORT: 9000,
-  PEER_PATH: "/puzzle-game",
-  YOUTUBE_API_KEY: "dev_key"
-};
